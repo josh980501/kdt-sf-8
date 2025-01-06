721768dd63360bb8099146eaff976090116d7085
josh980501 <ch032kr@gmail.com>
2025-01-06 14:38:07 +0900

    3.py 수정

--- code/python/3.py
+++ code/python/3.py
@@ -24,10 +24,19 @@
 # print("합계: " + str(total_score))
 # print("평균: " + str(avg_score))
 
+# #1번
+# name= input("이름을 입력하세요. ")
+# age = input("나이를 입력하세요. ")
+# print(f"안녕하세요! {name}님 ({age}세)")
+# print("안녕하세요! " + name + "님 (" + age + "세)")
+# print("안녕하세요!", name + "님", "(" + age + "세)")
 
 
-name= input("이름을 입력하세요. ")
-born = input("태어난 년도를 입력하세요. ")
-year = input("올해 년도를 입력하세요. ")
-age = int(year) - int(born) + 1
-print(f"올해는 {year}년, {name}님의 나이는 {age}세입니다")
+# #2번
+# name= input("이름을 입력하세요. ")
+# born = input("태어난 년도를 입력하세요. ")
+# year = input("올해 년도를 입력하세요. ")
+# age = int(year) - int(born) + 1
+# print(f"올해는 {year}년, {name}님의 나이는 {str(age)}세입니다") #print(f"")
+# print("올해는 " + year +"년, " + name + "님의 나이는 " + str(age) + "세입니다") #print(+)
+# print("올해는", year + "년,", name + "님의 나이는", str(age) + "세입니다") # print (, +)
